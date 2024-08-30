b8df7f83b93562e4318fcc4d8ad1ab4443a9c025
sanskar vegad <147686799+sanskar22003@users.noreply.github.com>
2024-08-30 10:06:00 +0530

    Update app.py

--- Part 3 UI Code/app.py
+++ Part 3 UI Code/app.py
@@ -88,6 +88,10 @@ def delete_from_cart():
 
     return jsonify(cart[user_id])
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
+    
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', debug=True)
 
